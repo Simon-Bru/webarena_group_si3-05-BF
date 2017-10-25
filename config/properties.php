f69b507a166041e1bf13c23b9b38d37b4a6cfff8
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2017-10-25 23:22:28 +0200

    Add of properties for app

---
+++ config/properties.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/**
+ * The Maximum xp for a fighter
+ */
+define('MAX_XP', 4);
+
+/**
+ * The Default health skill point for a fighter
+ */
+define('DEFAULT_SKILL_HEALTH', 5);
+
+/**
+ * The Default strength skill points for a fighter
+ */
+define('DEFAULT_SKILL_STRENGTH', 1);
+
+/**
+ * The Default strength skill points for a fighter
+ */
+define('DEFAULT_SKILL_SIGHT', 2);
+
+/**
+ * Arena width dimension
+ */
+define('ARENA_WIDTH', 15);
+
+/**
+ * Arena width dimension
+ */
+define('ARENA_HEIGHT', 10);
+
+?>
