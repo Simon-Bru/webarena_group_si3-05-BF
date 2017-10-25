b3e81fec4c77fe293343194956bfd1871612a877
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2017-10-25 14:02:25 +0200

    Add of welcome email

--- src/Mailer/PlayersMailer.php
+++ src/Mailer/PlayersMailer.php
@@ -9,7 +9,8 @@ class PlayersMailer extends Mailer
         $this
             ->to($player->email)
             ->setSubject(sprintf('Welcome !'))
-            ->setTemplate('welcome_mail', 'custom'); // Par défaut le template avec le même nom que le nom de la méthode est utilisé.
+            ->setTemplate('default')
+            ->set(['content' => 'Welcome to webarena! You can now login to fight to death !!']);
     }
 
     public function resetPassword($email, $pwd)
@@ -19,6 +20,21 @@ class PlayersMailer extends Mailer
             ->setSubject('WebArena - Password reinitialization')
             ->set(['password' => $pwd]);
     }
+
+    public function implementedEvents()
+    {
+        return [
+            'Model.afterSave' => 'onRegistration'
+        ];
+    }
+
+    public function onRegistration(Event $event, EntityInterface $entity, ArrayObject $options)
+    {
+        if ($entity->isNew()) {
+            $this->send('welcome', [$entity]);
+        }
+    }
+
 }
 
 ?>
